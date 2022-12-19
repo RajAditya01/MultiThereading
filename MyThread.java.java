0eb63a7e52258bed2f210237941c413d4130f83f
Aditya <adityavishal903@gmail.com>
2022-12-19 22:28:26 +0530

    InetAddress

--- MyThread.java.java
+++ MyThread.java.java
@@ -14,13 +14,12 @@ class MyThread implements Runnable
 		}
 	}
 
-	public static void main(String[] args) {
-		//create object of MyThread class
-		MyThread t1=new MyThread();
-		Thread thr=new Thread(t1);
-		//object of AnotherThread
-		MyAnotherThread t2=new MyAnotherThread();
-		thr.start();
-		t2.start();
+	// public static void main(String[] args) {
+	// 	//create object of MyThread class
+	// 	MyThread t1=new MyThread();
+	// 	Thread thr=new Thread(t1);
+	// 	//object of AnotherThread
+	// 	MyAnotherThread t2=new MyAnotherThread();
+	// 	thr.start();
+	// 	t2.start();
 	}
-}
